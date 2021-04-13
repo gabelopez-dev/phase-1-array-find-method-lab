460418790fd6552160b2be4e887d3487857905b0
Gabriel Lopez <gabelopez84@gmail.com>
2021-04-13 14:33:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+superbowlWin(record)
+Array.prototype.find()
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+  
